15c000fdb7ef60380e89302db2e4adfa950faa71
rohit raj <official.rohitr@gmail.com>
2022-01-18 15:27:22 +0530

    changed form validation from js to html

--- Checkout Page/regex.js
+++ Checkout Page/regex.js
@@ -1,74 +1,86 @@
-// variables
-const email = document.querySelector("#email")
-const phone = document.querySelector("#phone")
-const username = document.querySelector("#name")
-const address = document.querySelector("#address")
-const city = document.querySelector("#city")
-const postal = document.querySelector("#postal")
+// // variables
+// const email = document.querySelector("#email")
+// const phone = document.querySelector("#phone")
+// const username = document.querySelector("#name")
+// const address = document.querySelector("#address")
+// const city = document.querySelector("#city")
+// const postal = document.querySelector("#postal")
 
-// all needed regex 
+// // this variable checks if all the input fields are correct
+// var temp = 0;
 
-const email_regex = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/
+// // all needed regex 
 
-const phone_no = /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/
+// const email_regex = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/
 
-const string_reg = /[a-z][A-Z]/
+// const phone_no = /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/
 
-const num_reg = /^[0-9][0-9][0-9][0-9][0-9][0-9]$/
+// const string_reg = /[a-z][A-Z]/
 
-
-// testing the input for the regex
-
-
-// email validation
-email.addEventListener("blur", ()=>{
+// const num_reg = /^[0-9][0-9][0-9][0-9][0-9][0-9]$/
 
 
-    if(email_regex.test(email.value)){
-        console.log("correct email");
-    }
-    else{
-        alert("incorrect email")
-    }
-})
+// // testing the input for the regex
 
 
-// phone validation
-phone.addEventListener("blur", ()=>{
+// // email validation
+// email.addEventListener("blur", ()=>{
 
 
-    if(phone_no.test(phone.value)){
-        console.log("correct phone");
-    }
-    else{
-        alert("phone number should be of 10 digits only")
-    }
-})
+//     if(email_regex.test(email.value)){
+//         console.log("correct email");
+//         temp++
+//     }
+//     else{
+//         alert("incorrect email")
+//     }
+// })
 
 
-// name validation
-
-username.addEventListener("blur", ()=>{
-
-
-    if(string_reg.test(username.value)){
-        console.log("correct name");
-    }
-    else{
-        alert("Please use only letter while writing the name")
-    }
-})
-
-// postal code validation
+// // phone validation
+// phone.addEventListener("blur", ()=>{
 
 
-postal.addEventListener("blur", ()=>{
+//     if(phone_no.test(phone.value)){
+//         console.log("correct phone");
+//         temp++
+//     }
+//     else{
+//         alert("phone number should be of 10 digits only")
+//     }
+// })
 
 
-    if(num_reg.test(postal.value)){
-        console.log("correct postal");
-    }
-    else{
-        alert("The postal code should be only of 6 digits")
-    }
-})
+// // name validation
+
+// username.addEventListener("blur", ()=>{
+
+
+//     if(string_reg.test(username.value)){
+//         console.log("correct name");
+//         temp++
+//     }
+//     else{
+//         alert("Please use only letter while writing the name")
+//     }
+// })
+
+// // postal code validation
+
+
+// postal.addEventListener("blur", ()=>{
+
+
+//     if(num_reg.test(postal.value)){
+//         console.log("correct postal");
+//         temp++
+//     }
+//     else{
+//         alert("The postal code should be only of 6 digits")
+//     }
+// })
+
+// if(temp == 4)
+// {
+//     alert("Form submitted successfully")
+// }
